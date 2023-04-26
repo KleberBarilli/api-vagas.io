dc1af13eaa847c4d274b0eec46cbcb5043407c45
kleber.barilli <kleber.barilli73@gmail.com>
2023-04-26 13:03:15 -0300

    add mockito e primeiro test

--- src/test/java/com/br/jobs/JobsApplicationTests.java
+++ src/test/java/com/br/jobs/JobsApplicationTests.java
@@ -1,13 +1,48 @@
 package com.br.jobs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.br.jobs.domain.entities.Job;
+import com.br.jobs.domain.repositories.JobsRepository;
+import com.br.jobs.domain.services.CreateJobService;
 
 @SpringBootTest
 class JobsApplicationTests {
 
+	@InjectMocks
+	private CreateJobService createJobService;
+
+	@Mock
+	private JobsRepository jobsRepository;
+
 	@Test
-	void contextLoads() {
+	void shouldBeAbleToCreateJobOpportunity() {
+		Job job = new Job();
+		job.setCompany("Minha empresa");
+		job.setDescription("Descrição do trabalho");
+		job.setHardSkills(Arrays.asList("Java", "Spring"));
+		job.setSoftSkills(Arrays.asList("Comunicação", "Trabalho em equipe"));
+		job.setSalary(new BigDecimal("5000"));
+		job.setLocation("São Paulo");
+		job.setRemote(true);
+		job.setApplyLink("https://www.exemplo.com.br/vagas");
+		job.setOpen(true);
+
+		when(jobsRepository.insert(job)).thenReturn(job);
+
+		Job createdJob = createJobService.execute(job);
+
+		assertEquals(job, createdJob);
+
 	}
 
 }
